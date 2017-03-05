bd373a1662981439849fa40729b79893386caa31
Vedran Novakovic <vedran.novakovic@stfc.ac.uk>
2017-03-05 00:15:30 +0000

    small fixes

--- vn/example/xdmtxvis.sh
+++ vn/example/xdmtxvis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -v ../cmap/plt8/jet8.txt dmtxvis.plt
 ifort -mkl -standard-semantics -static-intel xdmtxvis.f90 -o xdmtxvis.exe -L../.. -lvn
+#gfortran-6 -O3 -march=native -Wa,-q -fopenmp xdmtxvis.f90 -o xdmtxvis.exe -L../.. -lvn -L$HOME/OpenBLAS/lib -lopenblas_omp
 ./xdmtxvis.exe
 cmp dmtxvis-0000000000.bmp dmtxvis.bmp
