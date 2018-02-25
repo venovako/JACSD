5ce8c02cbc054720a298abd887eec10e6a11abbf
Vedran Novakovic <venovako@venovako.eu>
2018-02-25 21:56:07 +0100

    mini test for normalizator

--- test/utils/nrmlz.c
+++ test/utils/nrmlz.c
@@ -10,7 +10,7 @@ float _Complex cnrmlz_(const float _Complex *const z)
   const long double z_re = (long double)crealf(*z);
   const long double z_im = (long double)cimagf(*z);
   const long double magn = hypotl(z_re, z_im);
-  return ((float)(z_re / magn) + _Complex_I * (float)(z_im / magn));
+  return ((float)(z_re / magn) + (float)(z_im / magn) * _Complex_I);
 }
 
 double _Complex znrmlz_(const double _Complex *const z)
@@ -19,5 +19,5 @@ double _Complex znrmlz_(const double _Complex *const z)
   const long double z_re = (long double)creal(*z);
   const long double z_im = (long double)cimag(*z);
   const long double magn = hypotl(z_re, z_im);
-  return ((double)(z_re / magn) + _Complex_I * (double)(z_im / magn));
+  return ((double)(z_re / magn) + (double)(z_im / magn) * _Complex_I);
 }
