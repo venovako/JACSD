dcc5b5d77a3cd7451b56549f6c0f7e59ae2b106b
Vedran Novakovic <venovako@venovako.eu>
2018-02-26 11:25:35 +0100

    disable MKL_DIRECT_CALL in C

--- x200.mk
+++ x200.mk
@@ -21,7 +21,7 @@ endif # ?USE_MPI
 CC=icc
 C11FLAGS=-DUSE_INTEL -DUSE_X200 -DVN_INTEGER_KIND=8 -std=c11 -fexceptions
 ifdef NDEBUG
-OPTFLAGS=-O$(NDEBUG) -xHost #-xMIC-AVX512
+OPTFLAGS=-O$(NDEBUG) -xHost -DMKL_DIRECT_CALL #-xMIC-AVX512
 OPTCFLAGS=-O$(NDEBUG) -xHost #-xMIC-AVX512
 DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -diag-disable=10397
 DBGCFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3 -diag-disable=1572,2547,10397
@@ -35,7 +35,7 @@ DBGCFLAGS=-g -debug emit_column -debug extended -debug inline-debug-info -debug 
 FPUFLAGS=-fma -fp-model source -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt #-fp-model strict -assume ieee_fpe_flags -fp-stack-check
 FPUCFLAGS=-fma -fp-model source -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt #-fp-model strict -fp-stack-check
 endif # ?NDEBUG
-LIBFLAGS=-DUSE_MKL -I. -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include -qopenmp #-DMKL_DIRECT_CALL
+LIBFLAGS=-DUSE_MKL -I. -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include -qopenmp
 LDFLAGS=-L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -ldl -lmemkind
 FCFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFLAGS)
 CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
