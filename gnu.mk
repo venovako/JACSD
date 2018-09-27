68949f533b2ae2be5237a282678c3703565963ee
Vedran Novakovic <venovako@venovako.eu>
2018-09-27 18:33:34 +0200

    Switch to clang on macOS.

--- gnu.mk
+++ gnu.mk
@@ -9,46 +9,49 @@ ifndef WP
 WP=10
 endif # !WP
 ARFLAGS=rsv
+CPUFLAGS=-DUSE_GNU -DUSE_X64
+FORFLAGS=-cpp $(CPUFLAGS) -ffree-line-length-none -fopenmp -fstack-arrays
 ifdef USE_MPI
 FC=mpifort
-FORFLAGS=-cpp -DUSE_GNU -DUSE_X64 -DUSE_MPI -ffree-line-length-none -fstack-arrays
+FORFLAGS += -DUSE_MPI
 else # no MPI
 ifeq ($(ARCH),Darwin)
 FC=gfortran-8
 else # Linux
 FC=gfortran
 endif # ?Darwin
-FORFLAGS=-cpp -DUSE_GNU -DUSE_X64 -ffree-line-length-none -fstack-arrays
 endif # ?USE_MPI
 ifdef ILP64
 FORFLAGS += -fdefault-integer-8
 endif # ILP64
 ifeq ($(ARCH),Darwin)
-CC=gcc-8
+CC=clang
+C11FLAGS=$(CPUFLAGS) -std=gnu17 -pthread
 else # Linux
 CC=gcc
+C11FLAGS=$(CPUFLAGS) -std=gnu11 -fopenmp
 endif # ?Darwin
-C11FLAGS=-DUSE_GNU -DUSE_X64 -std=gnu11
 ifdef ILP64
 C11FLAGS += -DVN_INTEGER_KIND=8
 endif # ILP64
 ifdef NDEBUG
 ifeq ($(ARCH),Darwin)
 OPTFLAGS=-O$(NDEBUG) -march=native -Wa,-q -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
-OPTCFLAGS=-O$(NDEBUG) -march=native -Wa,-q -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
+OPTCFLAGS=-O$(NDEBUG) -march=native -integrated-as
+DBGCFLAGS=-DNDEBUG
 else # Linux
 OPTFLAGS=-O$(NDEBUG) -march=native -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
 OPTCFLAGS=-O$(NDEBUG) -march=native -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
+DBGCFLAGS=-DNDEBUG -fopt-info-optimized-vec
 endif # ?Darwin
 DBGFLAGS=-DNDEBUG -fopt-info-optimized-vec -pedantic -Wall -Wextra -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
-DBGCFLAGS=-DNDEBUG -fopt-info-optimized-vec
 FPUFLAGS=-ffp-contract=fast
 FPUCFLAGS=-ffp-contract=fast -fno-math-errno
 OPTFLAGS += -DMKL_DIRECT_CALL
 else # DEBUG
 ifeq ($(ARCH),Darwin)
 OPTFLAGS=-Og -march=native -Wa,-q
-OPTCFLAGS=-Og -march=native -Wa,-q
+OPTCFLAGS=-Og -march=native -integrated-as
 else # Linux
 OPTFLAGS=-Og -march=native
 OPTCFLAGS=-Og -march=native
@@ -60,9 +63,9 @@ FPUCFLAGS=-ffp-contract=fast
 endif # ?NDEBUG
 LIBFLAGS=-DUSE_MKL -I.
 ifdef ILP64
-LIBFLAGS += -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include -fopenmp
+LIBFLAGS += -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include
 else # LP64
-LIBFLAGS += -I${MKLROOT}/include -fopenmp
+LIBFLAGS += -I${MKLROOT}/include
 endif # ?ILP64
 ifeq ($(ARCH),Darwin)
 LDFLAGS=-L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -L${MKLROOT}/../compiler/lib -Wl,-rpath,${MKLROOT}/../compiler/lib
