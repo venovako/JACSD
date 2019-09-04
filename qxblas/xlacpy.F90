2181fca5c5225aaf17bfa318cc6fae3c90cb4db3
Vedran Novakovic <venovako@venovako.eu>
2019-09-05 00:48:58 +0200

    Add XLACPY.

---
+++ qxblas/xlacpy.F90
@@ -0,0 +1,147 @@
+!> \brief \b XLACPY copies all or part of one two-dimensional array to another.
+!
+!  =========== DOCUMENTATION ===========
+!
+! Online html documentation available at
+!            http://www.netlib.org/lapack/explore-html/
+!
+!
+!  Definition:
+!  ===========
+!
+!       SUBROUTINE XLACPY( UPLO, M, N, A, LDA, B, LDB )
+!
+!       .. Scalar Arguments ..
+!       CHARACTER          UPLO
+!       INTEGER            LDA, LDB, M, N
+!       ..
+!       .. Array Arguments ..
+!       COMPLEX(WP)        A( LDA, * ), B( LDB, * )
+!       ..
+!
+!
+!> \par Purpose:
+!  =============
+!>
+!> \verbatim
+!>
+!> XLACPY copies all or part of a two-dimensional matrix A to another
+!> matrix B.
+!> \endverbatim
+!
+!  Arguments:
+!  ==========
+!
+!> \param[in] UPLO
+!> \verbatim
+!>          UPLO is CHARACTER*1
+!>          Specifies the part of the matrix A to be copied to B.
+!>          = 'U':      Upper triangular part
+!>          = 'L':      Lower triangular part
+!>          Otherwise:  All of the matrix A
+!> \endverbatim
+!>
+!> \param[in] M
+!> \verbatim
+!>          M is INTEGER
+!>          The number of rows of the matrix A.  M >= 0.
+!> \endverbatim
+!>
+!> \param[in] N
+!> \verbatim
+!>          N is INTEGER
+!>          The number of columns of the matrix A.  N >= 0.
+!> \endverbatim
+!>
+!> \param[in] A
+!> \verbatim
+!>          A is COMPLEX(WP) array, dimension (LDA,N)
+!>          The m by n matrix A.  If UPLO = 'U', only the upper trapezium
+!>          is accessed; if UPLO = 'L', only the lower trapezium is
+!>          accessed.
+!> \endverbatim
+!>
+!> \param[in] LDA
+!> \verbatim
+!>          LDA is INTEGER
+!>          The leading dimension of the array A.  LDA >= max(1,M).
+!> \endverbatim
+!>
+!> \param[out] B
+!> \verbatim
+!>          B is COMPLEX(WP) array, dimension (LDB,N)
+!>          On exit, B = A in the locations specified by UPLO.
+!> \endverbatim
+!>
+!> \param[in] LDB
+!> \verbatim
+!>          LDB is INTEGER
+!>          The leading dimension of the array B.  LDB >= max(1,M).
+!> \endverbatim
+!
+!  Authors:
+!  ========
+!
+!> \author Univ. of Tennessee
+!> \author Univ. of California Berkeley
+!> \author Univ. of Colorado Denver
+!> \author NAG Ltd.
+!
+!> \date December 2016
+!
+!> \ingroup complex16OTHERauxiliary
+!
+!  =====================================================================
+SUBROUTINE XLACPY(UPLO, M, N, A, LDA, B, LDB)
+!
+!  -- LAPACK auxiliary routine (version 3.7.0) --
+!  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+!  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+!     December 2016
+!
+  IMPLICIT NONE
+  INCLUDE 'qx_wp.fi'
+!     .. Scalar Arguments ..
+  CHARACTER, INTENT(IN) :: UPLO
+  INTEGER, INTENT(IN) :: LDA, LDB, M, N
+!     ..
+!     .. Array Arguments ..
+  COMPLEX(WP), INTENT(IN) :: A(LDA,*)
+  COMPLEX(WP), INTENT(OUT) :: B(LDB,*)
+!     ..
+!
+!  =====================================================================
+!
+!     .. Local Scalars ..
+  INTEGER :: I, J
+!     ..
+!     .. External Functions ..
+  LOGICAL, EXTERNAL :: LSAME
+!     ..
+!     .. Executable Statements ..
+!
+  IF (LSAME(UPLO, 'U')) THEN
+     DO J = 1, N
+        DO I = 1, MIN(J, M)
+           B(I,J) = A(I,J)
+        END DO
+     END DO
+!
+  ELSE IF (LSAME(UPLO, 'L')) THEN
+     DO J = 1, N
+        DO I = J, M
+           B(I,J) = A(I,J)
+        END DO
+     END DO
+!
+  ELSE
+     DO J = 1, N
+        DO I = 1, M
+           B(I,J) = A(I,J)
+        END DO
+     END DO
+  END IF
+!
+!     End of XLACPY
+!
+END SUBROUTINE XLACPY
