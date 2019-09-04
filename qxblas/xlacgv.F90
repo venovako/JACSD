9cbe6f6f550bfd95113c525aba64dcb92678b5f6
Vedran Novakovic <venovako@venovako.eu>
2019-09-05 00:48:11 +0200

    Clean up the comments.

--- qxblas/xlacgv.F90
+++ qxblas/xlacgv.F90
@@ -5,15 +5,6 @@
 ! Online html documentation available at
 !            http://www.netlib.org/lapack/explore-html/
 !
-!> \htmlonly
-!> Download XLACGV + dependencies
-!> <a href="http://www.netlib.org/cgi-bin/netlibfiles.tgz?format=tgz&filename=/lapack/lapack_routine/zlacgv.f">
-!> [TGZ]</a>
-!> <a href="http://www.netlib.org/cgi-bin/netlibfiles.zip?format=zip&filename=/lapack/lapack_routine/zlacgv.f">
-!> [ZIP]</a>
-!> <a href="http://www.netlib.org/cgi-bin/netlibfiles.txt?format=txt&filename=/lapack/lapack_routine/zlacgv.f">
-!> [TXT]</a>
-!> \endhtmlonly
 !
 !  Definition:
 !  ===========
