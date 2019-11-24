5957c0960841553c6bbebf8d411b47f6e535e19f
Vedran Novakovic <venovako@venovako.eu>
2019-11-24 21:26:50 +0100

    Update the compiler flags.

---
+++ gnu9.mk
@@ -0,0 +1,74 @@
+SHELL=/bin/bash
+ARCH=$(shell uname)
+ifndef WP
+WP=10
+endif # !WP
+ifdef NDEBUG
+DEBUG=
+else # DEBUG
+DEBUG=g
+endif # ?NDEBUG
+RM=rm -rfv
+AR=ar
+ARFLAGS=rsv
+CPUFLAGS=-DUSE_GNU -DUSE_X64 -fPIC -fexceptions -fno-omit-frame-pointer -fopenmp -rdynamic
+ifdef PROFILE
+CPUFLAGS += -DVN_PROFILE=$(PROFILE) -fno-inline -finstrument-functions
+endif # PROFILE
+FORFLAGS=-cpp $(CPUFLAGS) -fdefault-integer-8 -ffree-line-length-none -fstack-arrays
+C11FLAGS=$(CPUFLAGS) -std=gnu17
+ifeq ($(ARCH),Darwin)
+CC=gcc-9
+FC=gfortran-9
+else # Linux
+CC=gcc
+FC=gfortran
+endif # ?Darwin
+ifdef NDEBUG
+OPTFLAGS=-O$(NDEBUG) -march=native -fgcse-las -fgcse-sm -fipa-pta -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts -fvect-cost-model=unlimited -fvariable-expansion-in-unroller
+DBGFLAGS=-DNDEBUG -fopt-info-optimized-vec -pedantic -Wall -Wextra
+ifeq ($(ARCH),Darwin)
+OPTFLAGS += -Wa,-q
+endif # Darwin
+OPTFFLAGS=$(OPTFLAGS)
+OPTCFLAGS=$(OPTFLAGS)
+DBGFFLAGS=$(DBGFLAGS) -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
+DBGCFLAGS=$(DBGFLAGS)
+FPUFLAGS=-ffp-contract=fast
+FPUFFLAGS=$(FPUFLAGS)
+FPUCFLAGS=$(FPUFLAGS) #-fno-math-errno
+OPTFFLAGS += -DMKL_DIRECT_CALL
+else # DEBUG
+OPTFLAGS=-O$(DEBUG) -march=native
+DBGFLAGS=-$(DEBUG) -fsanitize=address -pedantic -Wall -Wextra
+ifeq ($(ARCH),Darwin)
+OPTFLAGS += -Wa,-q
+else # Linux
+DBGFLAGS += -fsanitize=leak
+endif # ?Darwin
+OPTFFLAGS=$(OPTFLAGS)
+OPTCFLAGS=$(OPTFLAGS)
+DBGFFLAGS=$(DBGFLAGS) -fcheck=all -finit-local-zero -finit-real=snan -finit-derived -Wno-compare-reals -Warray-temporaries -Wcharacter-truncation -Wimplicit-procedure -Wfunction-elimination -Wrealloc-lhs-all
+DBGCFLAGS=$(DBGFLAGS) -fsanitize=undefined #-ftrapv
+FPUFLAGS=-ffp-contract=fast
+FPUFFLAGS=$(FPUFLAGS) #-ffpe-trap=invalid,zero,overflow
+FPUCFLAGS=$(FPUFLAGS)
+endif # ?NDEBUG
+LIBFLAGS=-DUSE_MKL -DMKL_ILP64 -I. -I../vn -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include
+LDFLAGS=-L.. -lvn$(PROFILE)$(DEBUG)
+ifeq ($(ARCH),Darwin)
+LDFLAGS += -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -L${MKLROOT}/../compiler/lib -Wl,-rpath,${MKLROOT}/../compiler/lib -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5
+else # Linux
+LIBFLAGS += -D_GNU_SOURCE
+LDFLAGS += -L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_gf_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp
+endif # ?Darwin
+ifndef NDEBUG
+ifeq ($(ARCH),Darwin)
+LDFLAGS += -lubsan
+else # Linux
+$(error debug build currently not possible with RH devtoolset)
+endif # ?Darwin
+endif # DEBUG
+LDFLAGS += -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
+FFLAGS=$(OPTFFLAGS) $(DBGFFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFFLAGS)
+CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
