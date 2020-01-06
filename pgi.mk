d53d648c500803248a78931f4162634b766d23cc
Vedran Novakovic <venovako@venovako.eu>
2020-01-06 16:24:28 +0100

    Improve compatibility with the PGI compilers.

--- pgi.mk
+++ pgi.mk
@@ -1,8 +1,8 @@
 SHELL=/bin/bash
 ARCH=$(shell uname)
-ifeq ($(ARCH),Darwin)
-$(error PGI on macOS is not supported)
-endif # Darwin
+ifneq ($(ARCH),Linux)
+$(error PGI is supported on Linux only)
+endif # !Linux
 ifndef WP
 WP=8
 endif # !WP
@@ -15,9 +15,10 @@ RM=rm -rfv
 AR=ar
 ARFLAGS=rsv
 CPUFLAGS=-DUSE_PGI -DUSE_X64 -DKIND_QUAD=$(WP) -DOLD_OMP -m64 -mp -KPIC -Mframe -Meh_frame -Minfo -Mnollvm
-ifneq ($(ARCH),Darwin)
+ifdef PROFILE
+CPUFLAGS += -DVN_PROFILE=$(PROFILE) -Mnoinline -Minstrument=functions
+endif # PROFILE
 CPUFLAGS += -DTSC_FREQ_HZ=$(shell if [ `if [ -r /etc/redhat-release ]; then grep -c 'release 7' /etc/redhat-release; else echo 0; fi` = 1 ]; then echo `dmesg | grep 'TSC clocksource calibration' | cut -d':' -f3 | cut -d' ' -f2 | sed 's/\.//g'`000; else echo 0; fi)ull
-endif # Linux
 FORFLAGS=$(CPUFLAGS) -i8 -Mdclchk -Mlarge_arrays -Mrecursive -Mstack_arrays
 C11FLAGS=$(CPUFLAGS) -c11
 CC=pgcc
@@ -43,18 +44,11 @@ DBGCFLAGS=$(DBGFLAGS)
 FPUFFLAGS=$(FPUFLAGS)
 FPUCFLAGS=$(FPUFLAGS)
 endif # ?NDEBUG
-LIBFLAGS=-DUSE_MKL -DMKL_ILP64 -I. -I../vn -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include
-LDFLAGS=-L.. -lvn$(DEBUG)
 # define MKL=pgi_thread for a threaded MKL
 ifndef MKL
 MKL=sequential
 endif # !MKL
-ifeq ($(ARCH),Darwin)
-LDFLAGS += ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_$(MKL).a ${MKLROOT}/lib/libmkl_core.a
-else # Linux
-LIBFLAGS += -D_GNU_SOURCE
-LDFLAGS += -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_$(MKL) -lmkl_core
-endif # ?Darwin
-LDFLAGS += -pgf90libs -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
+LIBFLAGS=-D_GNU_SOURCE -DUSE_MKL -DMKL_ILP64 -I. -I../vn -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include
+LDFLAGS=-L.. -lvn$(PROFILE)$(DEBUG) -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_$(MKL) -lmkl_core -pgf90libs -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
 FFLAGS=$(OPTFFLAGS) $(DBGFFLAGS) $(LIBFLAGS) $(FORFLAGS) $(FPUFFLAGS)
 CFLAGS=$(OPTCFLAGS) $(DBGCFLAGS) $(LIBFLAGS) $(C11FLAGS) $(FPUCFLAGS)
