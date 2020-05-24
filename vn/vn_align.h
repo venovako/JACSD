a1697c347c9d891f758ed5acd1321de08bb40a78
Vedran Novakovic <venovako@venovako.eu>
2020-05-24 22:05:09 +0200

    Add VN_ASSUME_ALIGNED.

--- vn/vn_align.h
+++ vn/vn_align.h
@@ -9,6 +9,20 @@
 #define VN_ALIGN_BYTES VN_L1D_CLS_B
 #endif /* !VN_ALIGN_BYTES */
 
+#ifndef VN_ASSUME_ALIGNED
+#ifdef __ICC
+#define VN_ASSUME_ALIGNED(a) __assume_aligned((a), (VN_ALIGN_BYTES))
+#else /* !__ICC */
+#if (defined(__GNUC__) || defined(__clang__))
+#define VN_ASSUME_ALIGNED(a) (void)__builtin_assume_aligned((a), (VN_ALIGN_BYTES))
+#else /* !__GNUC__ && !__clang__ */
+#define VN_ASSUME_ALIGNED(a) (void)0
+#endif /* ?(__GNUC__ || __clang__ ) */
+#endif /* ?__ICC */
+#else /* VN_ASSUME_ALIGNED */
+#error VN_ASSUME_ALIGNED already defined
+#endif /* ?VN_ASSUME_ALIGNED */
+
 #ifndef VN_VAR_ALIGNED
 #if (defined(__GNUC__) || defined(__clang__))
 #define VN_VAR_ALIGNED __attribute__((aligned(VN_ALIGN_BYTES)))
@@ -32,10 +46,10 @@
 #endif /* ?__ICC */
 #else /* !NDEBUG */
 #define VN_IS_ALIGNED(a) VN_ASSERT(!((intptr_t)(a) % (VN_ALIGN_BYTES)))
-#endif /* NDEBUG */
+#endif /* ?NDEBUG */
 #else /* VN_IS_ALIGNED */
 #error VN_IS_ALIGNED already defined
-#endif /* !VN_IS_ALIGNED */
+#endif /* ?VN_IS_ALIGNED */
 
 #ifndef VN_IS_ALIGNED2
 #define VN_IS_ALIGNED2(A, ldA)                            \
@@ -43,6 +57,6 @@
   VN_ASSERT(!((ldA) % ((VN_ALIGN_BYTES) / sizeof(*(A)))))
 #else /* VN_IS_ALIGNED2 */
 #error VN_IS_ALIGNED2 already defined
-#endif /* !VN_IS_ALIGNED2 */
+#endif /* ?VN_IS_ALIGNED2 */
 
 #endif /* !VN_ALIGN_H */
